a7c6c04700e5f37295d5e0c1578d7823fc820797
Emily Rendleman <emilyrendleman@gmail.com>
2021-05-06 18:52:40 -0500

    Testing branches

--- assets/main.js
+++ assets/main.js
@@ -25,17 +25,3 @@ function getApi () {
 }
 getApi();
 
-// google search api - limits to 3 requests per hour, may be difficult to work with for that reason, does not include article images
-// fetch("https://google-news1.p.rapidapi.com/search?q=florida%20man&country=US&lang=en", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "c2dbb5a7fdmsha6ee8767c5acd11p1dee2ejsne2cc8e8d8536",
-// 		"x-rapidapi-host": "google-news1.p.rapidapi.com"
-// 	}
-// })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
